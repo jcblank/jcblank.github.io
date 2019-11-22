8fcdab71b34ce5172306d58e54a0fbf7ea05be93
jcblank <31256868+jcblank@users.noreply.github.com>
2019-11-21 23:21:36 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ for (let card of cards) {
         // const titId = card.getElementsByTagName("h3")
         // const textId = = card.getElementsByTagName("p")
         modalOverlay.classList.add('active') 
-        modalOverlay.querySelector("img").src = `https://github.com/jcblank/jcblank.github.io/tree/master/img`${fotoId}.png`
+        modalOverlay.querySelector("img").src = `https://github.com/jcblank/jcblank.github.io/tree/master/img/${fotoId}.png`    
         modalOverlay.querySelector(".modal-content h3").innerHTML = card.querySelector(".card1 p").textContent;
         modalOverlay.querySelector(".modal-content p").innerHTML = card.querySelector(".card1 h3").textContent;
     })
